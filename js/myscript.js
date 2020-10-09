73b1154b1f22ba77fcf83570fa3fb7a0cf0f047d
Katharina Simma <katharina.simma@diebin.at>
2020-10-09 18:07:18 +0200

    finish basic project

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,29 @@
 var movieData = JSON.parse(movieData);
-//console.table(movieData);           // for testing
-//console.log(movieData[1].name);     // for testing
 
 // include json data in html incl. bootstrap
 for (let i = 0; i < movieData.length; i++){
-    console.log($(".row"))
     $(".row").append(`
-        <div class="col col-md-6 movieBox">
-            <img src=${movieData[i].img}></img>
-            <ul>
-                <li>${movieData[i].name}</li>
-                <li>${movieData[i].year}</li>
-                <li>${movieData[i].description}</li>
-                <li>${movieData[i].likes}</li>
-            </ul>
+        <div class="col-lg-6 movieBox">
+        
+            <img src=${movieData[i].img} class="rounded"></img>
+
+            <div class="font-weight-light">
+                <div class="card-body">
+                <h5 class="card-title">${movieData[i].name}</h5>
+                <p class="card-text">${movieData[i].genre}, ${movieData[i].year}</p>
+                <p class="card-text">${movieData[i].description}</p><br>
+                <span >
+                    <i class="fas fa-thumbs-up"id=${i} ></i>
+                    <span >${movieData[i].likes}</span>              
+                </span>
+            </div>
         </div>   
-    `)
+    `);
 };
+
+// event listener for the like button + incrementing the number of likes
+$(".fa-thumbs-up").on('click', function(){
+    let index = $(this).attr("id");
+    let newLikes = movieData[index].likes += 1;
+    $(this).siblings().first().html(`${newLikes}`);
+});
